d2e29e6d2a38bb664f22328958c182906809f41c
silluana <silluana@gmail.com>
2025-07-02 14:38:34 -0300

    Primeiro commit com as configurações iniciais

---
+++ src/main/java/com/client/api/cardapiolog/CardapiologApiApplication.java
@@ -0,0 +1,13 @@
+package com.client.api.cardapiolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapiologApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapiologApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/client/api/cardapiolog/CardapiologApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.api.cardapiolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapiologApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
